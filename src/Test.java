b2e7901857f999cb123e010cd11e5516ce64bafb
Cristopher Castro <castro.inofuentes.cristopher@gmail.com>
2020-04-01 14:51:40 -0300

    Vigenere

--- src/Test.java
+++ src/Test.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
 public class Test {
+    public static void main(String[] args){
+        Vigenere texto = new Vigenere("Esto es un mensaje cifrado", "melon");
+        String txtCifrado = texto.Cifrar();
+        System.out.println(txtCifrado);
+        Vigenere descifrar = new Vigenere(txtCifrado, "melon");
+        String txtDescifrado = descifrar.DesCifrar();
+        System.out.println(txtDescifrado);
+    }
 }
